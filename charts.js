d862e1a11ca5b56bf83540a022bb64b49e990f6a
Jashjeet Singh <jashjeet99@gmail.com>
2026-05-13 23:05:46 +0530

    charts implementation done

--- charts.js
+++ charts.js
@@ -2,7 +2,7 @@ const strPerson = localStorage.getItem("usersDetails");
 const Persons = JSON.parse(strPerson);
 const sPersonEmail = sessionStorage.getItem("loginemail");
 let Person;
-let Expenses = [];
+let Expenses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 for (let user of Persons) {
     if (user.email === sPersonEmail) {
         Person = user;
@@ -11,7 +11,9 @@ for (let user of Persons) {
 if (Person) {
     for (let exp of Person.transactions) {
         if (exp.type === "Expense") {
-            Expenses.push(exp.amount);
+            const D = new Date(exp.date);
+            const date = Number(D.getMonth());
+            Expenses[date] = (Expenses[date] || 0) + exp.amount;
         }
     }
 }
@@ -19,18 +21,18 @@ const lineCanvas = document.querySelector("#lineChart");
 const chart = new Chart(lineCanvas, {
     type: "line",
     data: {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
         datasets: [
             {
                 label: "Monthly Expenses",
-                data: [5000, 7000, 4000, 9000, 6000, 11000],
+                data: Expenses,
                 borderColor: "#3b82f6",
                 backgroundColor: "rgba(59, 130, 246, 0.2)",
                 fill: true,
-                tension: 0.15,
+                tension: 0,
                 borderWidth: 3,
                 pointBackgroundColor: "#3b82f6",
-                pointRadius: 5
+                pointRadius: 0,
             }
         ]
     },
